bd7f041681086c1a6dafac8ec373935fb37d1c71
DAdarsh08 <112772798+DAdarsh08@users.noreply.github.com>
2023-01-28 07:38:03 -0800

    recursion

--- src/RECURSION/Palindrome.java
+++ src/RECURSION/Palindrome.java
@@ -2,11 +2,27 @@ package RECURSION;
 
 public class Palindrome {
     public static void main(String[] args) {
-        int n=1234;
+        int n=121;
         int sum=0;
-        palin(n,sum);
+       // palin(n,sum);
+        //palindrome(1234);
+        System.out.println(palindrome(n));
 
 
+    }
+
+    private static int  palindrome(int n) {
+        int  digit=(int)(Math.log10(n)+1);
+        return helper(n,digit);
+
+    }
+
+    private static int helper(int n, int digit) {
+        if(n%10==n)
+        {
+            return n;
+        }
+       return  (n%10)*(int)Math.pow(10,digit-1)+helper(n/10,digit-1);
     }
 
     public static void palin(int n,  int sum) {
